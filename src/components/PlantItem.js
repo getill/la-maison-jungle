b0797d4955cbaa3af6e807171efd50a29aad4f49
getill <98155180+getill@users.noreply.github.com>
2023-01-17 12:59:48 +0100

    This OpenClassroom course organisation is a mess..

--- src/components/PlantItem.js
+++ src/components/PlantItem.js
@@ -1,8 +1,21 @@
 import React from "react";
 import "../styles/PlantItem.css";
+import CareScale from "./CareScale";
 
 const PlantItem = ({ name, cover, id, light, water }) => {
-  return <div></div>;
+  function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix`);
+  }
+  return (
+    <li key={id} className="lmj-plant-item" onClick={() => handleClick(name)}>
+      <img className="lmj-plant-item-cover" src={cover} alt={`${name} cover`} />
+      {name}
+      <div>
+        <CareScale careType="water" scaleValue={water} />
+        <CareScale careType="light" scaleValue={light} />
+      </div>
+    </li>
+  );
 };
 
 export default PlantItem;
